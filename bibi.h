650916d4f88131e5925f40ec44689396f947b458
u-eag <47338122+u-eag@users.noreply.github.com>
2019-03-30 16:41:08 -0300

    Add files via upload

---
+++ bibi.h
@@ -0,0 +1,44 @@
+/** \brief Esta función calcula la suma de dos números enteros (x+y).
+ *
+ * \param Se ingresa un número entero.
+ * \param Devuelve un número entero.
+ * \return Devuelve la suma de dos enteros.
+ *
+ */
+int suma (int x, int y);
+
+/** \brief Esta función calcula la resta de dos números enteros (x-y).
+ *
+ * \param Se ingresa un número entero.
+ * \param Devuelve un número entero.
+ * \return Devuelve la resta de dos enteros.
+ *
+ */
+int resta (int x, int y);
+
+/** \brief Esta función calcula la división de dos números enteros (x/y).
+ *
+ * \param Se ingresa un número entero.
+ * \param Devuelve un número flotante.
+ * \return Devuelve la división de dos enteros.
+ *
+ */
+float division (int x, int y);
+
+/** \brief Esta función calcula la multiplicación de dos números enteros (x*y).
+ *
+ * \param Se ingresa un número entero.
+ * \param Devuelve un número entero.
+ * \return Devuelve la multiplicación de dos números enteros.
+ *
+ */
+int multiplicacion (int x, int y);
+
+/** \brief Esta función calcula el factorial de un número x (x!).
+ *
+ * \param Se ingresa un número entero.
+ * \param Devuelve un número entero.
+ * \return Devuelve el resultado del factorial si el número ingresado es igual o mayor a cero.
+ *         Devuelve -1 para indicar error si el número ingresado es negativo.
+ */
+int factorial (int x);
